dc6997109e8afba1e0ebfa4d172a9f14d8146d92
dreymotoh <striclyspoken@gmail.com>
2022-11-10 18:04:52 -0500

    first coding challenge!

---
+++ Returning Strings/8kyu.js
@@ -0,0 +1,12 @@
+// Title: Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function inputStatement(firstName) {
+    firstName = 'drey'
+    return `Hello, ${firstName} how are you doing today?`
+}
+
+inputStatement()
