f8bfc0108851040e7421898ae76ecede63aa4f37
dreymotoh <striclyspoken@gmail.com>
2022-11-30 11:32:57 -0500

    coding challenge

---
+++ string-to-num/string-to-number.js
@@ -0,0 +1,11 @@
+// Title: Convert a string to a number! 
+
+// declare if statement
+// return string as a number by wrapping the parameter with parseFloat()
+
+const stringToNumber = function(str){
+    if(str) {
+        return parseFloat(str)
+    }
+    return null;
+  }
