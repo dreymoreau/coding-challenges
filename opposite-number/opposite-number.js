2afd5a472207778f357ec75bbbae73bef6a14192
dreymotoh <striclyspoken@gmail.com>
2023-01-03 10:03:17 -0500

    coding challenge

---
+++ opposite-number/opposite-number.js
@@ -0,0 +1,11 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// depending on the inputted number, return its opposite
+// return either -number or +number
+function opposite(number) {
+    return -number
+    return +number
+  }
+  
+  console.log(opposite(3.33)) // output => -3.33
+  console.log(opposite(-8.88)) // output => 8.88
