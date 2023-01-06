1de0ca5ccc6076b564fe039ad1620ab4ff7c64df
dreymotoh <striclyspoken@gmail.com>
2023-01-06 17:43:28 -0500

    coding challenge!

--- filter-evens/filter-evens.js
+++ filter-evens/filter-evens.js
@@ -1,3 +1,7 @@
+//declare onlyEvens variable and assign it the numbersArray parameter using the filter method, passing in an argument that only selects
+// the elements that are divisible by 2 === 0 using the modulus
+// return the onlyEvens variable
+
 function getEvenNumbers(numbersArray){
     let onlyEvens = numbersArray.filter(x => x % 2 === 0)
     return onlyEvens
