f13ef5236feb401f30c16ac46c09dcc4a0c4668a
dreymotoh <striclyspoken@gmail.com>
2023-01-27 08:58:21 -0500

    coding challenge!

--- calculate-average/calculate-average.js
+++ calculate-average/calculate-average.js
@@ -1,21 +1,21 @@
 // Write a function which calculates the average of the numbers in a given list.
 
 // check if array.length === 0 return 0
-// declare sum variable to 0
+// declare total variable to 0
 // for loop through
-// add all elements together to sum variable
-// return sum // array.length to get the average of numbers given
+// add all element of array to total variable
+// return total // array.length to get the average of numbers given
 
 function findAverage(array) {
     if (array.length === 0) {
-        return 0;
-    }
-    let sum = 0;
-    for(let i = 0; i < array.length; i++) {
-        sum += array[i]
-    }
-    return sum / array.length
-    }
-    
-    console.log(findAverage([1,1,1]))
-    console.log(findAverage([1,2,3]))
+    return 0
+  }
+ let total = 0
+ for (let i = 0; i < array.length; i++){
+     total += array[i]
+ }
+ return total / array.length
+}
+
+console.log(findAverage([1,2,3,4])) // output => 2.5
+console.log(findAverage([])) // output => 0
