c3db48b28609e6d98ec47cc35ef33cda1d567486
dreymotoh <striclyspoken@gmail.com>
2023-01-31 19:22:42 -0500

    change

--- debug-sum-of-digits-number/debug-sum-of-digits-number.js
+++ debug-sum-of-digits-number/debug-sum-of-digits-number.js
@@ -4,7 +4,7 @@
 // use toString() method
 // then split('') the string into an array
 // map through the array to make sure each element is then changed into a number, using map(Number)
-// then use reduce method to dd all the elements inside of the array together into one number
+// then use reduce method to add all the elements inside of the array together into one number
 function getSumOfDigits(integer) {
     return integer.toString().split('').map(Number).reduce((acc, value) => acc + value)
     }
