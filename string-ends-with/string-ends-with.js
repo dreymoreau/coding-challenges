4d973270b1538c1de90cd72829df7dc4d244c9aa
dreymotoh <striclyspoken@gmail.com>
2023-02-03 09:29:55 -0500

    coding challenge!

---
+++ string-ends-with/string-ends-with.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// directly return and use endsWith() method, checking if str .endsWith () ending
+function solution(str, ending){
+    return str.endsWith(ending)
+  }
+  
+  console.log(solution('abcde', 'cde')) //true
