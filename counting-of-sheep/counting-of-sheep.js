39a8c8f9df067949f7b70d6a640eb2891b92dfc7
dreymotoh <striclyspoken@gmail.com>
2023-02-23 10:23:40 -0500

    coding challenge!

---
+++ counting-of-sheep/counting-of-sheep.js
@@ -0,0 +1,26 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// declare count variable
+// for loop through
+// if arrayOfSheep[i]
+// count ++
+// return count
+
+function countSheeps(arrayOfSheep) {
+    let count = 0;
+   for(let i = 0; i < arrayOfSheep.length; i++) {
+       if(arrayOfSheep[i]) {
+           count++
+       }
+   }
+    return count
+}
+
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ]
+              
+              console.log(countSheeps(array1))
