bd8a5bf2709852d62adc584bd07453de458552cb
dreymotoh <striclyspoken@gmail.com>
2023-03-15 10:05:04 -0400

    coding challenge

---
+++ credit-card-mask/credit-card-mask.js
@@ -0,0 +1,21 @@
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your 
+//most secret question is still correct. However, since someone could look over your shoulder, you don't want 
+//that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// split into an array
+// loop through, making condition i < cc.length - 4
+// cc[i] = '#'
+// cc = cc.join('')
+// return cc
+
+function maskify(cc) {
+    cc = cc.split('')
+    for(let i = 0; i < cc.length - 4; i++){
+        cc[i] = '#'
+    }
+    cc = cc.join('')
+    return cc
+ }
+ console.log(maskify('4556364607935616')) // output => '############5616'
