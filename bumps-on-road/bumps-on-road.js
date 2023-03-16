bc980512a875bc77428adbfc5aacd9fa946048ee
dreymotoh <striclyspoken@gmail.com>
2023-03-16 10:56:32 -0400

    coding challenge

--- bumps-on-road/bumps-on-road.js
+++ bumps-on-road/bumps-on-road.js
@@ -2,21 +2,22 @@
 
 // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
-// declare count variable to 0
+// declare count variable
 // for loop through
-// if x[i] === 'n', increment count by 1
-// outside of the for loop and if statement, using a ternary operator return if count is greater than 15 'Car Dead', if not 'Woohoo'
+// if x[i] === 'n' count++
+// return using ternary operator if count is greater than 15 ? 'Car Dead' : 'Woohoo!'
 function bump(x){
-    let count = 0
-     for(let i = 0; i < x.length; i++) {
-         if (x[i] === 'n'){
-             count++
-         }
-     }
-     return count > 15 ? 'Car Dead' : 'Woohoo!'
-    
-     // ** alternate way **
-  //   return x.split('n').length > 16 ? 'Car Dead' : 'Woohoo!'
-  }
-  
-  console.log(bump("__nn_nnnn__n_n___n____nn__nnn")) // => output 'Woohoo!'
+    let count = 0;
+    for(let i = 0; i < x.length; i++){
+        if(x[i] == 'n') {
+            count++
+        }
+    }
+    return count > 15 ? 'Car Dead' : 'Woohoo!'
+}
+
+// ** refactored way**
+bump = x => x.split('n').length > 15 ? 'Car Dead' : 'Woohoo'
+
+console.log(bump("__nn_nnnn__n_n___n____nn__nnn")) // => 'Woohoo!'
+console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")) // => 'Car Dead'
