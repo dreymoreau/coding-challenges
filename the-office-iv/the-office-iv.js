45e2b1de3b873fff94ae27a3384a5891dc9c72e5
dreymotoh <striclyspoken@gmail.com>
2023-05-24 11:37:21 -0400

    coding challenge

---
+++ the-office-iv/the-office-iv.js
@@ -0,0 +1,25 @@
+// Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+// In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+// 'X' --> busy
+// 'O' --> empty
+// If all rooms are busy, return "None available!"
+
+// for loop through
+// check if x[i] == 'O' return i
+// outside the for loop return 'None available' if all rooms are busy
+
+function meeting(x){
+    for(let i = 0; i < x.length; i++){
+       if(x[i] == 'O'){
+       return i 
+     }
+    }
+   return 'None available!'
+ }
+ 
+ 
+ console.log(meeting(['X', 'O', 'X'])) // 1);
+ console.log(meeting(['0','X','X','X','X'])) // 0);
+ console.log(meeting(['X','X','X','X','X'])) //'None available!');
