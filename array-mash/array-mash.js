faded84c7fea6d106aa0b80aa5326ceb04fcc893
dreymotoh <striclyspoken@gmail.com>
2023-11-21 11:33:57 -0500

    coding challenge

--- array-mash/array-mash.js
+++ array-mash/array-mash.js
@@ -2,16 +2,16 @@
 
 // eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
 
-// declare total variable assigning it to an empty array
+// declare finalArr variable assigning it to an empty array
 // for loop
 // push elements array1[i] , array2[i] to total array
-// return total
+// return finalArr
 
 function arrayMash (array1, array2) {
-    let total = []
+    let finalArr = []
     for(let i = 0; i < array1.length; i++){
-        total.push(array1[i],array2[i])
+      finalArr.push(array1[i],array2[i])
     }
-    return total
+    return finalArr
   }
-  console.log(arrayMash([1, 2, 3], ['a', 'b', 'c'])) // [1, 'a', 2, 'b', 3, 'c']
+  console.log(arrayMash([1,2,3], ["a", "b","c"]))
